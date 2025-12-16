6324d1c92bb849ce8fa59706e41cf2a6a11cffb3
juanpradana <suarahp@gmail.com>
2025-12-16 16:07:11 +0700

    fix(map): add Chrome options for headless mode

--- backend/app/map_generator.py
+++ backend/app/map_generator.py
@@ -138,6 +138,10 @@ class MapGenerator:
             chrome_options.add_argument('--headless')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-extensions')
+            chrome_options.add_argument('--disable-web-security')
+            chrome_options.add_argument('--allow-running-insecure-content')
             chrome_options.add_argument(f'--window-size={width},{height}')
             
             driver = webdriver.Chrome(options=chrome_options)
